126c77886271f5f87372698385aa6d5e00367aae
Diego Aranda <arandadiegoa@gmail.com>
2024-10-15 10:49:04 -0300

    Se agrega imagen

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ const Page = () => {
                 <meta property="og:description" content="Descripción de mi App." />
             </Head>
             <h1>Bienvenido a mi App</h1>
+            <img src=" https://www.fravega.com/centro-de-ayuda/_next/image?url=%2Fcentro-de-ayuda%2F_next%2Fstatic%2Fmedia%2Fimg-familia.4dfbfbb6.jpg&w=1920&q=75" alt="" />
         </>
     );
 };
